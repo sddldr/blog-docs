8b4a624a19135a02249288927b2498442d776a30
sddldr <1061579860@qq.com>
2021-02-25 22:11:45 +0800

    feat: 搭建初始目录结构

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,33 @@
 module.exports = {
     title: '在孤独中品尝苦涩',
+    lastUpdated: '最近更新时间',
     base: process.env.BUILD_ENV === 'develop' ? '/' : './',
     description: 'Just playing around',
     themeConfig: {
+        sidebarDepth: 2,
         nav: [
-            { text: 'Home', link: '/' },
-        ]
-    }
+            { text: '首页', link: '/' },
+            {
+              text: '技术博客',
+              ariaLabel: '技术博客',
+              items: [
+                { text: 'Webpack', link: '/tech/webpack/' },
+                { text: 'Vue', link: '/tech/vue/' }
+              ]
+            },
+            { text: '生活杂感', link: '/life/' },
+            { text: '优美诗词', link: '/goodPoem/' },
+        ],
+        sidebar: {
+          '/goodPoem/': [
+            '我有个想念的人',
+          ],
+          '/tech/webpack/': [
+            '/tech/webpack/ad'
+          ],
+          '/tech/vue/': [
+            '/tech/vue/vue3'
+          ]
+        }
+    },
 }
